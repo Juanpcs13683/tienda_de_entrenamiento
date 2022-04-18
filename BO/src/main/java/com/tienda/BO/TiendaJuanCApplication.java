bbe29978f00eaa75eae24eb7b68ad204ab6c6381
Juanpcs13683 <84336310+Juanpcs13683@users.noreply.github.com>
2022-04-18 17:20:30 -0500

    primer commit
    proyecto inicializado con spring initializer.

---
+++ BO/src/main/java/com/tienda/BO/TiendaJuanCApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.BO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJuanCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJuanCApplication.class, args);
+	}
+
+}
---
+++ BO/src/test/java/com/tienda/BO/TiendaJuanCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.BO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJuanCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
